68e93ba4195640ba4cca08e76de38e74eb28e533
Karan Kumar Chile <91055905+Karan9512@users.noreply.github.com>
2024-11-08 15:23:31 +0530

    Update Navbar.jsx

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
-import { Link } from 'react-router-dom';
+import { navLinks } from '../constants/index.js';
 
-const NavItems = () => {
-    return (
-        <ul className="nav-ul">
-            <li className="nav-li"><Link to="/">Home</Link></li>
-            <li className="nav-li"><Link to="/about">About</Link></li>
-            <li className="nav-li"><Link to="/projects">Projects</Link></li>
-            {/*<li className="nav-li"><Link to="/clients">Clients</Link></li>*/}
-            <li className="nav-li"><Link to="/experience">Experience</Link></li>
-            <li className="nav-li"><Link to="/contact">Contact</Link></li>
-        </ul>
-    );
-};
+const NavItems = ({ onClick = () => {} }) => (
+    <ul className="nav-ul">
+        {navLinks.map((item) => (
+            <li key={item.id} className="nav-li">
+                <a href={item.href} className="nav-li_a" onClick={onClick}>
+                    {item.name}
+                </a>
+            </li>
+        ))}
+    </ul>
+);
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
