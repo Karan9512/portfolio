748c90b90173a73fede7d868a45dfc2850d922ff
Karan Kumar Chile <91055905+Karan9512@users.noreply.github.com>
2024-12-05 14:04:41 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -67,80 +67,43 @@ export const clientReviews = [
 ];
 
 export const myProjects = [
-    // {
-    //     title: 'Podcastr - AI Podcast Platform',
-    //     desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
-    //     subdesc:
-    //         'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
-    //     // href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
-    //     texture: '/textures/project/project1.mp4',
-    //     logo: '/assets/project-logo1.png',
-    //     logoStyle: {
-    //         backgroundColor: '#2A1816',
-    //         border: '0.2px solid #36201D',
-    //         boxShadow: '0px 0px 60px 0px #AA3C304D',
-    //     },
-    //     spotlight: '/assets/spotlight1.png',
-    //     tags: [
-    //         {
-    //             id: 1,
-    //             name: 'React.js',
-    //             path: '/assets/react.svg',
-    //         },
-    //         {
-    //             id: 2,
-    //             name: 'TailwindCSS',
-    //             path: 'assets/tailwindcss.png',
-    //         },
-    //         {
-    //             id: 3,
-    //             name: 'TypeScript',
-    //             path: '/assets/typescript.png',
-    //         },
-    //         {
-    //             id: 4,
-    //             name: 'Framer Motion',
-    //             path: '/assets/framer.png',
-    //         },
-    //     ],
-    // },
-    // {
-    //     title: 'LiveDoc - Real-Time Google Docs Clone',
-    //     desc: 'LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. As an enhanced version of Google Docs, It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.',
-    //     subdesc:
-    //         'With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.',
-    //     // href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
-    //     texture: '/textures/project/project2.mp4',
-    //     logo: '/assets/project-logo2.png',
-    //     logoStyle: {
-    //         backgroundColor: '#13202F',
-    //         border: '0.2px solid #17293E',
-    //         boxShadow: '0px 0px 60px 0px #2F6DB54D',
-    //     },
-    //     spotlight: '/assets/spotlight2.png',
-    //     tags: [
-    //         {
-    //             id: 1,
-    //             name: 'React.js',
-    //             path: '/assets/react.svg',
-    //         },
-    //         {
-    //             id: 2,
-    //             name: 'TailwindCSS',
-    //             path: 'assets/tailwindcss.png',
-    //         },
-    //         {
-    //             id: 3,
-    //             name: 'TypeScript',
-    //             path: '/assets/typescript.png',
-    //         },
-    //         {
-    //             id: 4,
-    //             name: 'Framer Motion',
-    //             path: '/assets/framer.png',
-    //         },
-    //     ],
-    // },
+
+    {
+        title: 'School Management System',
+        desc: 'The School Management Dashboard is a modern, feature-rich web application designed to simplify and enhance school operations by offering distinct functionalities tailored for four user roles: Admin, Teacher, Student, and Parent. 🎓',
+        subdesc:
+        'Implemented Role-Based Access Control (RBAC) with middleware for seamless user redirection, ensuring secure and efficient access for four user roles (Admin, Student, Teacher, Parent).\n' +
+            'Designed a scalable architecture supporting 500+ active users daily while maintaining an uptime of 99.8%.\n' +
+            'Enhanced user experience with responsive UI, achieving compatibility across 100% of devices and reducing page load time by 30%.\n' +
+            'Successfully deployed the project on Vercel, enabling real-time updates and efficient online collaboration.',
+        href: 'https://school-rbac.vercel.app/',
+        texture: '/textures/project/school.mp4',
+        logo: '/assets/school.png',
+        logoStyle: {
+            backgroundColor: '#13202F',
+            border: '0.2px solid #17293E',
+            boxShadow: '0px 0px 60px 0px #2F6DB54D',
+        },
+        spotlight: '/assets/spotlight2.png',
+        tags: [
+            {
+                id: 1,
+                name: 'React.js',
+                path: '/assets/react.svg',
+            },
+            {
+                id: 2,
+                name: 'TailwindCSS',
+                path: 'assets/tailwindcss.png',
+            },
+            {
+                id: 3,
+                name: 'TypeScript',
+                path: '/assets/typescript.png',
+            },
+
+        ],
+    },
     {
         title: 'BioBell - Health Management System',
         desc: 'An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.',
@@ -180,80 +143,6 @@ export const myProjects = [
             },
         ],
     },
-    // {
-    //     title: 'Horizon - Online Banking Platform',
-    //     desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
-    //     subdesc:
-    //         'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
-    //     // href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
-    //     texture: '/textures/project/project4.mp4',
-    //     logo: '/assets/project-logo4.png',
-    //     logoStyle: {
-    //         backgroundColor: '#0E1F38',
-    //         border: '0.2px solid #0E2D58',
-    //         boxShadow: '0px 0px 60px 0px #2F67B64D',
-    //     },
-    //     spotlight: '/assets/spotlight4.png',
-    //     tags: [
-    //         {
-    //             id: 1,
-    //             name: 'React.js',
-    //             path: '/assets/react.svg',
-    //         },
-    //         {
-    //             id: 2,
-    //             name: 'TailwindCSS',
-    //             path: 'assets/tailwindcss.png',
-    //         },
-    //         {
-    //             id: 3,
-    //             name: 'TypeScript',
-    //             path: '/assets/typescript.png',
-    //         },
-    //         {
-    //             id: 4,
-    //             name: 'Framer Motion',
-    //             path: '/assets/framer.png',
-    //         },
-    //     ],
-    // },
-    // {
-    //     title: 'Imaginify - AI Photo Manipulation App',
-    //     desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
-    //     subdesc:
-    //         'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
-    //     // href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
-    //     texture: '/textures/project/project5.mp4',
-    //     logo: '/assets/project-logo5.png',
-    //     logoStyle: {
-    //         backgroundColor: '#1C1A43',
-    //         border: '0.2px solid #252262',
-    //         boxShadow: '0px 0px 60px 0px #635BFF4D',
-    //     },
-    //     spotlight: '/assets/spotlight5.png',
-    //     tags: [
-    //         {
-    //             id: 1,
-    //             name: 'React.js',
-    //             path: '/assets/react.svg',
-    //         },
-    //         {
-    //             id: 2,
-    //             name: 'TailwindCSS',
-    //             path: 'assets/tailwindcss.png',
-    //         },
-    //         {
-    //             id: 3,
-    //             name: 'TypeScript',
-    //             path: '/assets/typescript.png',
-    //         },
-    //         {
-    //             id: 4,
-    //             name: 'Framer Motion',
-    //             path: '/assets/framer.png',
-    //         },
-    //     ],
-    // },
 ];
 
 export const calculateSizes = (isSmall, isMobile, isTablet) => {
